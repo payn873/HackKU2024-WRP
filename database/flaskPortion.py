a82a41518c5d39761f5fc14be989ca375cc2b89c
payn873 <142923202+payn873@users.noreply.github.com>
2024-04-13 22:18:23 -0500

    Update flaskPortion.py
    Added comments

--- database/flaskPortion.py
+++ database/flaskPortion.py
@@ -4,27 +4,33 @@ Creates urls for website
 from flask import Flask, request
 
 app = Flask(__name__)
+#Reads html text files for menu and search
 fMenu = ''
 with open('Menu.txt', 'r') as f:
     fMenu = f.read()
 fSearch = ''
 with open('search.txt', 'r') as f:
     fSearch = f.read()
+#Sets up menu web page
 @app.route('/')
 def menu():
     fileText = ''
     for line in fMenu:
         fileText += line
     return fileText
+#Sets up search web page for destocking
 @app.route('/search/')
 def store():
     return fSearch
+#Sets up storage web page
 @app.route('/storage/')
 def storage():
     return "Storage"
+#Sets up another search webpage for restocking
 @app.route('/restock/')
 def restock():
     return fSearch
+#Creates webpage for item being searched
 @app.route(f'/search/?search={request.form[search]}')
 def stock():
     return 'success'
